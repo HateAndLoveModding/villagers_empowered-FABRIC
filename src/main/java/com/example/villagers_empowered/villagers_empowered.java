ac9441f944fb343ffde12c4df0244c83b90db8ff
HateAndLoveModding <jymesryk@gmail.com>
2023-08-06 22:43:41 -0600

    Updated 1.20_1.20.1 to v1.2.2

--- src/main/java/com/example/villagers_empowered/villagers_empowered.java
+++ src/main/java/com/example/villagers_empowered/villagers_empowered.java
@@ -4,10 +4,19 @@ import com.example.villagers_empowered.block.ModBlocks;
 import com.example.villagers_empowered.item.ModItems;
 import com.example.villagers_empowered.villager.ModVillagers;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.village.VillagerProfession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class villagers_empowered implements ModInitializer {
+    //savanna and snowy
+    //fix height of structure
     public static final String MOD_ID = "villagers_empowered";
     public static final Logger LOGGER = LoggerFactory.getLogger("villagers_empowered");
 
